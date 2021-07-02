77677373cedb8d27fe3cbc2e8cfa04b888dd656a
yejin21 <83255075+yejin21@users.noreply.github.com>
2021-07-02 13:10:52 +0900

    나선로캣

--- Assets/Scripts/HSP_Scripts/MouseAttack.cs
+++ Assets/Scripts/HSP_Scripts/MouseAttack.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class MouseAttack : MonoBehaviour
 {
     public GameObject spiralRocket;
-    public Transform firePosition;
+    public GameObject firePosition;
     public float coolTime = 5.0f;
+    public float locketSpeed = 10.0f;
+    
+    Vector3 dir;
+    Vector3 crossDir;
+    GameObject go;
+    RaycastHit rHitInfo;
 
     float elapsedTime = 0;
 
     void Start()
     {
+        go = null;
         // 경과시간에 쿨타임을 넣고
         elapsedTime = coolTime;
+
+        // 정면 방향을 만들고
+        dir = new Vector3(0, 0, 1);
+
+        // 메인 카메라의 정면 방향으로 나아가고 싶다.
+        dir = Camera.main.transform.forward;
     }
 
     void Update()
@@ -40,10 +53,24 @@ public class MouseAttack : MonoBehaviour
         if (Input.GetMouseButtonDown(1) && elapsedTime >= coolTime)
         {
             // 레이를 생성한다.
-            
+            Ray rRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            // 레이에 부딪힌 오브젝트가 있다면
+            if (Physics.Raycast(rRay, out rHitInfo))
+            {
+                print(rHitInfo.transform.name);
+
+                // 그 오브젝트의 정보를 받은 포지션으로 방향을 만들고.
+                crossDir = rHitInfo.point - firePosition.transform.position;
+
+            }
+            else
+            {
+                // 카메라의 정면 방향으로 이동하고 싶다.
+                transform.position += dir * locketSpeed * Time.deltaTime;
+            }
             
             // 총알 프리팹을 생성한다.
-            GameObject go = Instantiate(spiralRocket);
+            go = Instantiate(spiralRocket);
 
             // 총알을 firePosition으로 회전시킨다.
             go.transform.rotation = firePosition.transform.rotation;
@@ -52,6 +79,10 @@ public class MouseAttack : MonoBehaviour
 
             // 경과시간을 초기화한다.
             elapsedTime = 0;
+
         }
+        // 그 방향으로 움직인다.
+        if(go != null)
+        go.transform.position += crossDir * locketSpeed * Time.deltaTime;
     }
 }
--- Assets/Scripts/HSP_Scripts/SpiralRocketMove.cs
+++ Assets/Scripts/HSP_Scripts/SpiralRocketMove.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class SpiralRocketMove : MonoBehaviour
 {
-    public float locketSpeed = 10.0f;
-
-    Vector3 dir;
+    
 
     void Start()
     {
-        // 정면 방향을 만들고
-        dir = new Vector3(0, 0, 1);
-
-        // 메인 카메라의 정면 방향으로 나아가고 싶다.
-        dir = Camera.main.transform.forward;
+        
     }
 
     void Update()
     {
-        // 이동하고 싶다.
-        transform.position += dir * locketSpeed * Time.deltaTime;
+        
     }
 }
