295b422a41fbf8c40bafec066caf934b27764389
yejin21 <83255075+yejin21@users.noreply.github.com>
2021-07-09 13:18:19 +0900

    우클릭 프리팹 파괴하고, 봇 데미지 주기 구현

--- Assets/Scripts/HSP_Scripts/SpiralRocket.cs
+++ Assets/Scripts/HSP_Scripts/SpiralRocket.cs
@@ -1,11 +1,22 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class SpiralRocket : MonoBehaviour
-//{
-//    private void OnCollisionEnter(Collision collision)
-//    {
-//        Destroy(gameObject);
-//    }
-//}
+public class SpiralRocket : MonoBehaviour
+{
+    public BotHPBarScript botHP;
+
+    private void Start()
+    {
+        botHP = GameObject.Find("Bot").GetComponent<BotHPBarScript>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Bot"))
+        {
+            botHP.BotGetDamaged(5);
+        }
+        Destroy(gameObject);
+    }
+}
